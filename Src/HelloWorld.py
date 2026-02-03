7f6c5cd2d51f44e9686a39ab1798a16107abe49f
ratulp-admin <ratulp.learning@gmail.com>
2026-02-04 05:00:16 +0530

    Addded new pracice code

--- Src/HelloWorld.py
+++ Src/HelloWorld.py
@@ -1,4 +1,14 @@
 import sys
+from typing import Type
+
 print ("hello world !")
 print ("Version:", sys.version)
 print ("Version Info:", sys.version_info)
+a= "Hello World"
+print (a[0:4])
+print("Python is \"awesome\"")
+A=(1,2,3,4,5)
+print(A[1:4])
+A = [1]
+A.append([2, 3, 4, 5])
+print (len(A))
